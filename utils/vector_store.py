26a83a9e6fd7dfe53723241aaca428ce693b9977
srivarsha0501 <152888541+srivarsha0501@users.noreply.github.com>
2026-02-26 14:23:55 +0530

    Update vector_store.py

--- utils/vector_store.py
+++ utils/vector_store.py
@@ -1,5 +1,5 @@
 # utils/vector_store.py
-
+import os
 import faiss
 import numpy as np
 
@@ -13,12 +13,16 @@ def create_faiss_index(vectors):
 
 
 def save_index(index, path="vector_db/resume.index"):
+    
+    folder = os.path.dirname(path)
+
+    if folder and not os.path.exists(folder):os.makedirs(folder,exist_ok=True)
     faiss.write_index(index, path)
-
-
+    
 def load_index(path="vector_db/resume.index"):
     return faiss.read_index(path)
 
 def search_index(index, query_vector, top_k=3):
     distances, indices = index.search(query_vector.astype("float32"), top_k)
+
     return indices[0]
